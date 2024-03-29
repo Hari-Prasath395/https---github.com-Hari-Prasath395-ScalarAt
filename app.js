6ddab293988879e8e549a8158fcbd85094373fc1
Hari-Prasath395 <hariiprasathh395@gmail.com>
2024-03-29 18:47:40 +0530

    latest 12

--- app.js
+++ app.js
@@ -1,51 +1,9 @@
-// let searchForm = document.querySelector(".search-form");
-// document.querySelector("#search-btn").onclick = () => {
-//   searchForm.classList.toggle("active");
-// };
-
-// window.onscroll = () => {
-//   searchForm.classList.remove("active");
-// };
-
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-//     if (this.scrollY > 5) {
-//       $(".navbar-below").addClass("sticky");
-//     } else {
-//       $(".navbar-bottom").removeClass("sticky");
-//     }
-//   });
-  
-//   $(".owl-carousel").owlCarousel({
-//     margin: 5,
-//     navigation: true,
-//     loop: true,
-//     autoplay: true,
-//     autoplayTimeout: 2000,
-//     autoplayHoverPause: true,
-
-//     responsive: {
-//       0: {
-//         items: 1,
-//         nav: false,
-//       },
-//       600: {
-//         items: 2,
-//         nav: false,
-//       },
-//       1000: {
-//         items: 3,
-//         nav: false,
-//       },
-//     },
-//   });
-// }); // close the function and fix the parentheses
 
 (function () {
   emailjs.init("uiZvbGXfPUacc3ebz");
 })();
 
-/*********************************************************************************************/
+
 function sendEmail(event) {
   event.preventDefault();
 
@@ -71,15 +29,14 @@ function sendEmail(event) {
       console.log("Email sent successfully:", response);
       alert("Thank you! Your message has been sent.");
       document.getElementById("contact-form").reset();
+      
     })
     .catch(function (error) {
       console.error("Email sending failed:", error);
       alert("Oops! Something went wrong. Please try again later.");
     });
-}
 
-document.getElementById("contact-form").addEventListener("submit", sendEmail);
-
+  }
 
 function scrollToTopic(topicId) {
   var element = document.getElementById(topicId);
